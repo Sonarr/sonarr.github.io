b8485c993d12db6ad9bcfd406b12c6100db34a2f
kay.one <kay.one@gmail.com>
2013-06-19 22:58:44 -0700

    fixed jquery.carouFredSel

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ require.config({
         '$'                   : 'JsLibraries/jquery',
         'underscore'          : 'JsLibraries/underscore',
         'marionette'          : 'JsLibraries/backbone.marionette',
+        'jquery.carouFredSel' : 'JsLibraries/jquery.carouFredSel',
         'libs'                : 'JsLibraries/'
     },
 
@@ -21,6 +22,11 @@ require.config({
 
         $: {
             exports: '$'
+        },
+
+
+        'jquery.carouFredSel':{
+            deps: ['$']
         },
 
         bootstrap: {
